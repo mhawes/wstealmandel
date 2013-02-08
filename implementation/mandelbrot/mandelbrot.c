320922b62749284ac56dae7fa020e60443506d16
mhawes <hawesmartin@googlemail.com>
2013-02-08 15:58:00 +0000

    Fixed serial version to allow large arrays. Removed 'out.ppm' as it can get quite large.

--- implementation/mandelbrot/mandelbrot.c
+++ implementation/mandelbrot/mandelbrot.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 #include "mandelbrot.h"
 
 /* -------------------------------------------------------------------------- */
@@ -16,13 +14,6 @@ main()
     /* ------------------------------------- */
     initialise( &c_max, &c_min, &c_factor);
     
-    Complex c;
-    
-    c.re = 0;
-    c.im = -0.1;
-    
-    printf("res = %c\n", is_member( &c));
-    
     compute_plane( &c_max, &c_min, &c_factor);
 
     return (0);
@@ -32,18 +23,14 @@ main()
 void initialise( Complex *c_max, Complex *c_min, Complex *c_factor)
 {
     c_min->re = -2.0;
-    c_min->im = -1.2;
-    
     c_max->re = 1.0;
+
+    c_min->im = -1.2;
     c_max->im = c_min->im + (c_max->re - c_min->re) * HEIGHT / WIDTH;
 
-    /* used to convert x, y of the raster plane into a complex */
+    /* used to convert x, y of the raster plane into a complex number */
     c_factor->re = (c_max->re - c_min->re) / (WIDTH - 1);
     c_factor->im = (c_max->im - c_min->im) / (HEIGHT - 1);
-    
-    printf("min: ");    print_complex( c_min);
-    printf("max: ");    print_complex( c_max);
-    printf("factor: "); print_complex( c_factor);
 }
 
 /* -------------------------------------------------------------------------- */
@@ -69,36 +56,24 @@ void compute_plane( Complex *c_max,
 {
     unsigned int x, y;
     Complex c_cur;      /* used as the current z value */
-    char plane[WIDTH][HEIGHT];  /* array to hold the generated image */
     
-    FILE *fp = fopen("out.ppm", "w+");
-    fprintf(fp, "P2\n%d %d\n15\n", WIDTH, HEIGHT);
-    
+    static char plane[HEIGHT][WIDTH];  /* array to hold the generated image */
+
     /* classic nested for loop approach */
-    for(y = 0; y < WIDTH; ++y)    
+    for(y = 0; y < HEIGHT; y++)    
     {
         c_cur.im = convert_y_coord( c_max->im, c_factor->im, y);
-        for(x = 0; x < HEIGHT; ++x)
+        
+        for(x = 0; x < WIDTH; x++)
         {
             c_cur.re = convert_x_coord( c_min->re, c_factor->re, x);
             
-            //print_complex(&c_cur);
-            
-            plane[x][y] = is_member( &c_cur);
-            //printf("%c ", plane[x][y]);
-            
-            if( plane[x][y] == '*'){
-                fprintf(fp, "1   ");
-            }
-            else{
-                fprintf(fp, "255 ");
-            }
+            plane[y][x] = is_member( &c_cur);
         }
-        fprintf(fp, "\n");
-        //printf("\n");
     }
     
-    fclose(fp);    
+    /* write it out to a PPM file */
+    write_to_ppm( plane);
 }
 
 /* -------------------------------------------------------------------------- */
@@ -110,25 +85,19 @@ char is_member(Complex *c)
     z.re = c->re; 
     z.im = c->im;
 
-    for(i = 0; i < MAX_ITERATIONS; ++i)
+    for(i = 0; i < MAX_ITERATIONS; i++)
     {
-        //printf("i=%d ", i);
-        //print_complex( &z);
-        
-        //printf("res=%g ",((z.re * z.re) + (z.im * z.im)));
         if( ((z.re * z.re) + (z.im * z.im)) > 4)
         {
             /* c is not a member of the set */
-            return ' ';
+            return PPM_WHITE;
         }
+        
         z = julia_func( &z, c);
-        //printf("\n");
     }
     
-    //printf("\n");
-    
     /* if it gets this far c is a member of the set */
-    return '*';
+    return PPM_BLACK;
 }
 
 /* -------------------------------------------------------------------------- */
@@ -148,14 +117,30 @@ Complex julia_func(Complex *z, Complex *c)
 }
 
 /* -------------------------------------------------------------------------- */
-/* DEBUG FUNCTIONS:                                                           */
+/* UTIL FUNCTIONS:                                                           */
 /* -------------------------------------------------------------------------- */
 void print_complex(Complex *com)
 {
     printf("(%g + %g*i) ", com->re, com->im);
 }
 
-void write_to_ppm( char plane[WIDTH][HEIGHT])
+/* -------------------------------------------------------------------------- */
+void write_to_ppm( char plane[HEIGHT][WIDTH])
 {
+    unsigned int x, y;
+
+    FILE *fp = fopen("out.ppm", "w+");
+    fprintf(fp, "P2\n%d %d\n1\n", WIDTH, HEIGHT);
     
+    /* classic nested for loop approach */
+    for(y = 0; y < HEIGHT; ++y)    
+    {
+        for(x = 0; x < WIDTH; ++x)
+        {
+            fprintf(fp, "%i ", plane[y][x]);
+        }
+        fprintf(fp, "\n");
+    }
+    
+    fclose(fp); 
 }
--- implementation/mandelbrot/mandelbrot.h
+++ implementation/mandelbrot/mandelbrot.h
@@ -1,13 +1,20 @@
-#define HEIGHT 2890
-#define WIDTH  2890
+#include <stdio.h>
+#include <math.h>
 
-#define MAX_ITERATIONS 30
+#define HEIGHT 8000
+#define WIDTH  10000
+
+#define PPM_WHITE 1
+#define PPM_BLACK 0
+
+#define MAX_ITERATIONS 25
 
 /* struct for nicely containing Complex numbers */
 typedef struct Complex{
     double re, im;
 } Complex;
 
+/* function declarations */
 
 void initialise                 ( Complex *, Complex *, Complex *);
 inline double convert_x_coord   ( double, double, unsigned int);
@@ -16,6 +23,6 @@ void compute_plane              ( Complex *, Complex *, Complex *);
 char is_member                  ( Complex *);
 Complex julia_func              ( Complex *, Complex *);
 
-/* DEBUG FUNCTIONS */
+/* UTIL FUNCTIONS */
 void print_complex              ( Complex *);
-void write_to_ppm               ( char[WIDTH][HEIGHT]);
+void write_to_ppm               ( char[HEIGHT][WIDTH]);
